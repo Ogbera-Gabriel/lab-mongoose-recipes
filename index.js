11042e4263bb052fbe063788525869ee98734fc4
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2023-08-03 17:33:11 +0100

    Lab-Done

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const data = require('./data');
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 //Method 1 : Using Async Await
+// Drop the unique index on the title field
+
+
 
 const manageRecipes = async () => {
   try {
@@ -15,9 +18,44 @@ const manageRecipes = async () => {
     const dbConnection = await mongoose.connect(MONGODB_URI);
     console.log(`Connected to the database: "${dbConnection.connection.name}"`);
 
+    
     // Before adding any recipes to the database, let's remove all existing ones
-    await Recipe.deleteMany();
+    // Drop the unique index on the title field
+    await Recipe.deleteMany({});
+    await Recipe.collection.dropIndexes();
 
+
+    const recipeDetail = {
+      title: 'Butter Muffin',
+      level: 'Easy Peasy',
+      ingredients: ['250g self-rasing flour', '1 tsp bicarbonate of soda', '150g chocolate chips', '100g golden caster sugar', '2 eggs', '150ml natural yogurt', '100g unsalted butter'],
+      cuisine: 'British',
+      image: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chocolate-chip-muffins-ccc8450.jpg?quality=90&webp=true&resize=440,400',
+      duration: 25,
+      creator: 'Lulu Grimes',
+    }
+    const recipeExample = await Recipe.create(recipeDetail);
+    console.log('Recipe is:', recipeExample.title)
+    // const dataRecipes = await Recipe.insertMany(data);
+    // dataRecipes.forEach((items) => console.log(items.title));
+    const uniqueData = Array.from(new Set(data.map((recipe) => recipe.title))).map((title) => data.find((recipe) => recipe.title === title));
+    const dataRecipes = await Recipe.insertMany(uniqueData);
+    dataRecipes.forEach((items) => console.log(items.title))
+
+    const changeRecipeData = await Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100})
+    if (changeRecipeData) {
+      console.log('success')
+    }else console.log('failed')
+
+    
+
+    const removeRecipeData = await Recipe.deleteOne({title: 'Carrot Cake'});
+    if (removeRecipeData) {
+      console.log('success')
+    } else console.log('failed')
+    
+    const closedConnection = await mongoose.connection.close(MONGODB_URI);
+    
     // Run your code here, after you have insured that the connection was made
   } catch (error) {
     console.log(error);
@@ -42,3 +80,6 @@ manageRecipes();
   .catch((error) => {
     console.error('Error connecting to the database', error);
   }); */
+
+   
+
